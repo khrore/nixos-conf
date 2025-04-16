28057988faa99878b9260b22d70540b4b65450f2
qrore <129329817+K0T0WARU@users.noreply.github.com>
2025-04-16 17:05:30 +0000

    changed yazi conf

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,7 +1,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "K0T0WARU";
+    userName = "qrore";
     userEmail = "129329817+K0T0WARU@users.noreply.github.com";
   };
 }
--- home-manager/modules/yazi/default.nix
+++ home-manager/modules/yazi/default.nix
@@ -6,28 +6,28 @@
   programs.yazi = {
     enable = true;
 
-    # Changing working directory when exiting Yazi
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableNushellIntegration = true;
     enableFishIntegration = true;
+
     package = pkgs-unstable.yazi;
+
     theme =
       builtins.fromTOML (builtins.readFile "${inputs.catppuccin-yazi}/themes/mocha/catppuccin-mocha-blue.toml")
       // {
-        # manage = {
-        #   syntect_theme = "~/.config/bat/themes/catppuccin-mocha.tmTheme";
-        # };
+        manage = {
+          syntect_theme = "~/.config/bat/themes/catppuccin-mocha.tmTheme";
+        };
       };
     settings = {
       manager = {
         sort_dir_first = true;
+        show_hidden = true;
+        linemode = "size";
       };
     };
   };
 
-  # catppuccin-mocha theme from yazi-flavors
-  # Catppuccin-mocha.tmTheme
-  # xdg.configFile."yazi/Catppuccin-mocha.tmTheme" = "${inputs.catppuccin-bat}/themes/Catppuccin Mocha.tmTheme";
-  # xdg.configFile."yazi/theme.toml".source = "${inputs.catppuccin-yazi}/themes/mocha/catppuccin-mocha-blue.toml";
+  # xdg.configFile."yazi/init.lua".source = ./init.lua;
 }
