9e4b4ad00d7a16ea70457e7ccb2a0e1ceadf5a80
khrore <129329817+khrore@users.noreply.github.com>
2025-05-07 10:46:12 +0000

    some comments

--- home-manager/modules/hyprland/programs/waybar/default.nix
+++ home-manager/modules/hyprland/programs/waybar/default.nix
@@ -4,5 +4,6 @@
     style = builtins.readFile ./style.css;
   };
 
+  # settings parameter is very annoying, so im just put config in system
   xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
 }
