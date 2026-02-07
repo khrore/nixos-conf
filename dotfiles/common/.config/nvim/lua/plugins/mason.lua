0bb1383fb500e7f1666a1184de305d7e0784aa9d
khrore <super.stas.0978@gmail.com>
2026-02-07 10:52:23 +0300

    mason added

---
+++ dotfiles/common/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		enabled = false,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		enable = false,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		enabled = false,
+	},
+}
