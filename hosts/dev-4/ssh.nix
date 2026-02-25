31f64295e022721c8c25e91211cde6c724cb59af
khrore <super.stas.0978@gmail.com>
2026-02-25 19:27:24 +0300

    fffffffffffffffff

--- hosts/dev-4/ssh.nix
+++ hosts/dev-4/ssh.nix
@@ -1,8 +1,5 @@
 { username, inputs, ... }:
 {
-  # Ensure target dir exists before secret materialization.
-  systemd.tmpfiles.rules = [ "d /home/${username}/.ssh 0700 ${username} users -" ];
-
   # Decrypt macix private key for dev-4 -> macix SSH access.
   age.secrets.macix_ssh_key = {
     file = inputs.secrets + "/keys/macix_ssh_key.age";
